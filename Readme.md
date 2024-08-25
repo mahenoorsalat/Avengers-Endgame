55c918340fc53b07b3322070da8a9e9884fe61f8
Mahenoor <salatmahenoor7.8.6@gmail.com>
2024-08-25 12:42:08 +0530

    added

---
+++ Readme.md
@@ -0,0 +1,27 @@
+![Project Banner](https://images8.alphacoders.com/101/thumbbig-1012707.webp)
+
+# Image Slider Project
+
+## Description
+
+This project is a simple yet visually appealing image slider, inspired by popular games such as "God of War," "The Witcher," "Watch Dog 2," and "Assassin's Creed." The slider allows users to navigate through different game-themed slides vertically using up and down buttons.
+
+## Features
+
+- **Vertical Sliding:** Navigate through the slides using up and down buttons.
+- **Responsive Design:** The slider adjusts to different screen sizes.
+- **Smooth Transitions:** Uses CSS for smooth transition effects between slides.
+
+## Technologies Used
+
+- **HTML:** For the basic structure of the web page.
+- **CSS:** For styling the slider and ensuring responsive design.
+- **JavaScript:** For slide navigation logic.
+
+## Installation
+
+To use this project locally, follow these steps:
+
+1. **Clone the repository:**
+   ```bash
+   git clone https://github.com/mahenoorsalat/Avengers-Endgame.git
---
+++ script.js
@@ -0,0 +1,28 @@
+const slider = document.querySelector('.slider-container');
+const left = document.querySelector('.left-slide');
+const right = document.querySelector('.right-slide');
+const Ubtn = document.getElementById('up-btn');
+const Dbtn = document.getElementById('down-btn');
+
+const slideLength = right.querySelectorAll('div').length;
+let activeSlideIndex = 0;
+
+function nextSlide(direction) {
+    const sliderHeight = slider.clientHeight;
+    if (direction === 'up') {
+        activeSlideIndex++;
+        if (activeSlideIndex > slideLength - 1) {
+            activeSlideIndex = 0;
+        }
+    } else if (direction === 'down') {
+        activeSlideIndex--;
+        if (activeSlideIndex < 0) {
+            activeSlideIndex = slideLength - 1;
+        }
+    }
+    right.style.transform = `translateY(-${activeSlideIndex * sliderHeight}px)`;
+    left.style.transform = `translateY(-${activeSlideIndex * sliderHeight}px)`;
+}
+
+Dbtn.addEventListener('click', () => nextSlide('up'));
+Ubtn.addEventListener('click', () => nextSlide('down'));
